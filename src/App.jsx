f932adfd5e6c5155ccf8b03ab53e00fc28f0c6c5
mahamudul-01 <mahamudul.hasan.mehedi.01@gmail.com>
2025-01-04 21:06:05 +0600

    use components

--- src/App.jsx
+++ src/App.jsx
@@ -6,8 +6,21 @@ function App() {
   return (
     <>
       <h1>Mahamudul Hasan mehedi</h1>
+      <Person></Person>
+      <Person></Person>
+      <Person></Person>
     </>
   )
+}
+
+// using a component
+function Person(){
+  const age=30;
+  const money=50;
+  const personDetails={name:'Mahamudul', age:age}
+ return(
+  <h3>The man whose name is {personDetails.name} and age {personDetails.age}</h3>
+ )
 }
 
 export default App
